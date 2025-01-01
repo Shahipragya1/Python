070df6dfd80a7f79f61d696f6b6357b8cc49d415
Shahipragya1 <133870716+Shahipragya1@users.noreply.github.com>
2025-01-01 22:21:55 +0545

    Create day3.py

---
+++ day3.py
@@ -0,0 +1,8 @@
+yourFood = input("Name a food: ")
+yourPlant = input("Name a type of plant: ")
+yourCooking = input("Name a method of cooking: ")
+yourBurned = input("What word describes burned food?: ")
+yourDIY = input("Name a DIY item: ")
+print()
+print("MENU")
+print(yourCooking, yourFood, "with", yourBurned, yourPlant, "on a bed of", yourDIY)
