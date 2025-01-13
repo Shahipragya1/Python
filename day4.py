a5bf378ce3497074d87732a1131d625d5dbdb7ad
Shahipragya1 <133870716+Shahipragya1@users.noreply.github.com>
2025-01-14 00:19:42 +0545

    Update day4.py

--- day4.py
+++ day4.py
@@ -1,1 +1,15 @@
+print("=== Your Adventure Simulator ===")
+print ("""You'll be asked a bunch of questions
+then we'll make you up an amazing
+story with YOU as the star! 🤩""")
+print()
+name = input("Your name: ")
+enemy = input("Your worst enemy's name: ")
+superPower = input("Your super power: ")
+print()
+print("Suddenly I am dancing in the rain with this guy",name)
+print("\033[31m'Our final battle begins!'\033[0m
+shouts the evil", enemy, "clearly missing the fact that", 
+name, "has the power of\033[35m", superPower, "\033[0mwhich means they'll win quite
+easily")
 
